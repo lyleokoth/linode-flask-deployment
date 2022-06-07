17248237999501060c7e70631336753fdd3a2a7a
lyleokoth <okothlyleochieng@gmail.com>
2022-06-07 18:10:49 +0300

    fix: fixed gunicornlaunch.

--- api/__init__.py
+++ api/__init__.py
@@ -13,32 +13,27 @@ from .helpers import are_flask_environment_variable_set
 
 load_dotenv()
 
-
-def create_app():
-    """Create the Flask app."""
-    if are_flask_environment_variable_set():
-        app = Flask(__name__)
-        app.register_blueprint(default)
-
-        if os.getenv('FLASK_ENV') == 'production':  # pragma: no cover
-            app.config.from_object('api.config.ProductionConfig')
-        elif os.getenv('FLASK_ENV') == 'development':  # pragma: no cover
-            app.config.from_object('api.config.DevelopmentConfig')
-        elif os.getenv('FLASK_ENV') == 'stage':  # pragma: no cover
-            app.config.from_object('api.config.StagingConfig')
-        elif os.getenv('FLASK_ENV') == 'test':
-            app.config.from_object('api.config.TestingConfig')
-        else:
-            app.config.from_object('api.config.DevelopmentConfig')
-
-        print(f"The configuration used is for {os.environ['FLASK_ENV']} environment.")
-        print(f"The database connection string is {app.config['SQLALCHEMY_DATABASE_URI']}.")
-
-        db.init_app(app=app)
-        migrate = Migrate()
-        migrate.init_app(app, db)
-
-        return app
-
+if not are_flask_environment_variable_set():
     print('Application existing...')
     sys.exit(1)
+
+app = Flask(__name__)
+app.register_blueprint(default)
+
+if os.getenv('FLASK_ENV') == 'production':  # pragma: no cover
+    app.config.from_object('api.config.ProductionConfig')
+elif os.getenv('FLASK_ENV') == 'development':  # pragma: no cover
+    app.config.from_object('api.config.DevelopmentConfig')
+elif os.getenv('FLASK_ENV') == 'stage':  # pragma: no cover
+    app.config.from_object('api.config.StagingConfig')
+elif os.getenv('FLASK_ENV') == 'test':
+    app.config.from_object('api.config.TestingConfig')
+else:
+    app.config.from_object('api.config.DevelopmentConfig')
+
+print(f"The configuration used is for {os.environ['FLASK_ENV']} environment.")
+print(f"The database connection string is {app.config['SQLALCHEMY_DATABASE_URI']}.")
+
+db.init_app(app=app)
+migrate = Migrate()
+migrate.init_app(app, db)
